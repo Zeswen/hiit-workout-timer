04030cbfbe4ee1619524f21b2173149b2b590e9f
Zeswen <me@zeswen.dev>
2021-12-18 13:18:42 +0100

    Add countdown to exercise and variables in description

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+// Refs
 const timerRef = document.querySelector("#timer");
 const workoutRef = document.querySelector("#workout");
 const resetRef = document.querySelector("#reset");
 const restRef = document.querySelector("#rest");
+
 const exerciseSecondsInputRef = document.querySelector("#exercise-seconds");
 const exerciseCountInputRef = document.querySelector("#exercise-count");
 const restSecondsInputRef = document.querySelector("#rest-seconds");
+
+const exerciseSecondsSpanRefs = document.querySelectorAll(".exercise-seconds");
+const exerciseCountSpanRefs = document.querySelectorAll(".exercise-count");
+const restSecondsSpanRefs = document.querySelectorAll(".rest-seconds");
 
 const beep = new Audio("assets/beep.mp3");
 const boop = new Audio("assets/boop.mp3");
@@ -18,20 +24,48 @@ let restSeconds = 120;
 let timer = 0;
 let rest = false;
 let interval;
+let countdownInterval;
+let countdown = 3;
 
 const padNum = (num) => `0${parseInt(num)}`.slice(-2);
 
-const startTimer = () => {
-  interval = createInterval();
+const processCountdown = (resolve) => {
+  if (countdown === 0) {
+    resolve();
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+  } else {
+    workoutRef.innerHTML = countdown;
+    countdown -= 1;
+  }
+};
+
+const startCountdown = async () => {
+  await new Promise((resolve) => {
+    countdown = 3;
+
+    countdownInterval = setInterval(() => {
+      processCountdown(resolve);
+    }, 1000);
+    processCountdown(resolve);
+  });
+};
+
+const startTimer = async () => {
+  rest = false;
   exerciseSecondsInputRef.disabled = true;
   exerciseCountInputRef.disabled = true;
   restSecondsInputRef.disabled = true;
+  await startCountdown();
+  interval = createInterval();
   workoutRef.innerHTML = "Stop";
 };
 
 const stopTimer = () => {
   clearInterval(interval);
+  clearInterval(countdownInterval);
   interval = null;
+  countdownInterval = null;
   workoutRef.innerHTML = "Start";
 };
 
@@ -49,8 +83,14 @@ const setTimerText = () => {
   timerRef.innerHTML = `${minutes}:${seconds}`;
 };
 
-const createInterval = () =>
-  setInterval(() => {
+const setRest = (resting) => {
+  rest = resting;
+  restRef.innerHTML = resting ? "Resting." : "Not resting.";
+};
+
+const createInterval = () => {
+  beep.play();
+  return setInterval(() => {
     timer += 1;
     setTimerText();
 
@@ -59,20 +99,19 @@ const createInterval = () =>
     const set = Math.floor(timer / setAndRestSeconds);
 
     if (timer % setAndRestSeconds === 0) {
-      rest = false;
-      restRef.innerHTML = "Not resting.";
+      setRest(false);
       beep.play();
     } else if (timer % (set * setAndRestSeconds + setSeconds) === 0) {
-      rest = true;
-      restRef.innerHTML = "Resting.";
+      setRest(true);
       boop.play();
     } else if (!rest && timer % (set * restSeconds + exerciseSeconds) === 0) {
       beep.play();
     }
   }, 1000);
+};
 
 const handleWorkoutButtonPress = () => {
-  if (interval) {
+  if (interval || countdownInterval) {
     stopTimer();
   } else {
     startTimer();
@@ -84,29 +123,51 @@ const handleResetButtonPress = () => {
   resetTimer();
 };
 
-const setTexts = () => {
+const setSpanValue = (node, value, suffix) => {
+  node.innerHTML = `${value} ${suffix}`;
+};
+
+const initTexts = () => {
   setTimerText();
-  console.log(exerciseSecondsInputRef.innerHTML);
   exerciseSecondsInputRef.value = exerciseSeconds.toString();
   exerciseCountInputRef.value = exerciseCount.toString();
   restSecondsInputRef.value = restSeconds.toString();
+  exerciseSecondsSpanRefs.forEach((node) => {
+    setSpanValue(node, exerciseSeconds.toString(), "seconds");
+  });
+  exerciseCountSpanRefs.forEach((node) => {
+    setSpanValue(node, exerciseCount.toString(), "exercises");
+  });
+  restSecondsSpanRefs.forEach((node) => {
+    setSpanValue(node, restSeconds.toString(), "seconds");
+  });
 };
 
-const setEventListeners = () => {
+const initEventListeners = () => {
   exerciseSecondsInputRef.addEventListener("input", (e) => {
-    exerciseSeconds = Number(e.target.value);
+    const { value } = e.target;
+    exerciseSeconds = Number(value);
+    exerciseSecondsSpanRefs.forEach((node) =>
+      setSpanValue(node, value, "seconds")
+    );
   });
   exerciseCountInputRef.addEventListener("input", (e) => {
-    exerciseCount = Number(e.target.value);
+    const { value } = e.target;
+    exerciseCount = Number(value);
+    exerciseCountSpanRefs.forEach((node) =>
+      setSpanValue(node, value, "exercises")
+    );
   });
   restSecondsInputRef.addEventListener("input", (e) => {
-    restSeconds = Number(e.target.value);
+    const { value } = e.target;
+    restSeconds = Number(value);
+    restSecondsSpanRefs.forEach((node) => setSpanValue(node, value, "seconds"));
   });
 };
 
 const setup = () => {
-  setTexts();
-  setEventListeners();
+  initTexts();
+  initEventListeners();
 };
 
 setup();
